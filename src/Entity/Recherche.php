f847515a2eca02025981f8a081818e57e2c93131
MarlonBas <drefir42@gmail.com>
2023-05-17 10:28:56 +0200

    untracked fixed

---
+++ src/Entity/Recherche.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RechercheRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=RechercheRepository::class)
+ */
+class Recherche
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $campus;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $stringRecherche;
+
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     */
+    private $dateDebut;
+
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     */
+    private $dateFin;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $checkOrganisateur;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $checkInscrit;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $checkNonInscrit;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $checkPassee;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCampus(): ?string
+    {
+        return $this->campus;
+    }
+
+    public function setCampus(string $campus): self
+    {
+        $this->campus = $campus;
+
+        return $this;
+    }
+
+    public function getStringRecherche(): ?string
+    {
+        return $this->stringRecherche;
+    }
+
+    public function setStringRecherche(string $stringRecherche): self
+    {
+        $this->stringRecherche = $stringRecherche;
+
+        return $this;
+    }
+
+    public function getDateDebut(): ?\DateTimeInterface
+    {
+        return $this->dateDebut;
+    }
+
+    public function setDateDebut(?\DateTimeInterface $dateDebut): self
+    {
+        $this->dateDebut = $dateDebut;
+
+        return $this;
+    }
+
+    public function getDateFin(): ?\DateTimeInterface
+    {
+        return $this->dateFin;
+    }
+
+    public function setDateFin(?\DateTimeInterface $dateFin): self
+    {
+        $this->dateFin = $dateFin;
+
+        return $this;
+    }
+
+    public function isCheckOrganisateur(): ?bool
+    {
+        return $this->checkOrganisateur;
+    }
+
+    public function setCheckOrganisateur(bool $checkOrganisateur): self
+    {
+        $this->checkOrganisateur = $checkOrganisateur;
+
+        return $this;
+    }
+
+    public function isCheckInscrit(): ?bool
+    {
+        return $this->checkInscrit;
+    }
+
+    public function setCheckInscrit(bool $checkInscrit): self
+    {
+        $this->checkInscrit = $checkInscrit;
+
+        return $this;
+    }
+
+    public function isCheckNonInscrit(): ?bool
+    {
+        return $this->checkNonInscrit;
+    }
+
+    public function setCheckNonInscrit(bool $checkNonInscrit): self
+    {
+        $this->checkNonInscrit = $checkNonInscrit;
+
+        return $this;
+    }
+
+    public function isCheckPassee(): ?bool
+    {
+        return $this->checkPassee;
+    }
+
+    public function setCheckPassee(bool $checkPassee): self
+    {
+        $this->checkPassee = $checkPassee;
+
+        return $this;
+    }
+}
